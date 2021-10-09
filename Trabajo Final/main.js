1b1e7e7252c6e848572aa255a09535ff31f097b5
CristianBiasutto <69542909+CristianBiasutto@users.noreply.github.com>
2021-10-09 13:48:26 -0300

    Add files via upload

---
+++ Trabajo Final/main.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.header');
+ScrollReveal().reveal('.seccion1', {delay: 500});
+ScrollReveal().reveal('.seccion2', {delay: 500});
+ScrollReveal().reveal('.seccion3', {delay: 500});
+ScrollReveal().reveal('.carrousel', {delay: 500});
+ScrollReveal().reveal('.video', {delay: 500});
+ScrollReveal().reveal('.seccion4', {delay: 500});
+ScrollReveal().reveal('footer', {delay: 500});
